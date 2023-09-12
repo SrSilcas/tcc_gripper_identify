fbb6f4962d3f3ff77cfb459fa1fe6b2acdb1cdfd
SrSilcas <lsac@cin.ufpe.br>
2023-09-12 11:55:35 -0300

    feat main_tests

---
+++ tests_main.py
@@ -0,0 +1,8 @@
+from robot_module import robot_singleton
+from robot_module import write_into_json
+if __name__ == '__main__':
+    robot_singleton.connect()
+    for i in range (1, 11):
+        list_destruction = robot_singleton.close_destruction()
+        write_into_json(list_destruction, i)
+        robot_singleton.open_tool()
