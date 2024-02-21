4982991469bd8f2e60709c1d02844374806ca9d2
Lucas Silva <97846615+SrSilcas@users.noreply.github.com>
2024-02-21 09:28:51 -0300

    Update tests_main.py

--- tests_main.py
+++ tests_main.py
@@ -1,7 +1,7 @@
 import csv
 import time
+import statistics
 from robot_module import robot_singleton
-from robot_module import write_into_txt
 
 
 def write_into_csv(dates, csv_name):
@@ -11,13 +11,56 @@ def write_into_csv(dates, csv_name):
 
 
 if __name__ == '__main__':
-    csv_name = '.csv'
+
+    csv_overall_name = 'colistemato_overall_2024_02_21.csv'
+    csv_statistics_name = 'colistemato_statistics_2024_02_21.csv'
+
     robot = robot_singleton()
     robot.connect()
-    write_into_csv(('response', 'position', 'deviation', 'average'), csv_name)
+
+    try:
+        arqv = open(csv_overall_name)
+        arqv.close
+
+    except:
+        write_into_csv(('response', 'position', 'deviation', 'average'), csv_overall_name)
+    
+    try:
+        arqv = open(csv_statistics_name)
+        arqv.close
+
+    except:
+        write_into_csv(('P deviation', 'P average', 'P median', 'D deviation', 'D average', 'D median', 'A deviation', 'A average', 'A median'), csv_statistics_name)
+    
     count = 0
+    positions = []
+    deviations = []
+    averages = []
+
     while count < 100:
         robot.open_tool(0.6)
+
+        time.sleep(0.16)
+        
         identification, position, deviation, average = robot.close_tool()
-        write_into_csv((identification, position, deviation, average), csv_name)
+        
+        write_into_csv((identification, position, deviation, average), csv_overall_name)
+                
+        positions.append(position)
+        deviations.append(deviation)
+        averages.append(average)
+
         count += 1
+
+
+
+    p_deviation = statistics.stdev(positions)
+    p_average = statistics.mean(positions)
+    p_median = statistics.median(positions)
+    d_deviation = statistics.stdev(deviations)
+    d_average = statistics.mean(deviations)
+    d_median = statistics.median(deviations)
+    a_deviation = statistics.stdev(averages)
+    a_average = statistics.mean(averages)
+    a_median = statistics.median(averages)
+    write_into_csv((p_deviation, p_average, p_median, d_deviation, d_average, d_median, a_deviation, a_average, a_median), csv_statistics_name)
